4b59b99717e176900701c3449b1decc33fbb3abd
Anzaur95 <anzaur-1992@mail.ru>
2024-07-26 10:13:23 +0300

    Add files via upload

---
+++ homework2_rev2_26.07.2024.py
@@ -0,0 +1,5 @@
+count_DZ = 12
+take_time = 1.5
+name = 'Python'
+task_time = take_time / count_DZ
+print('Курс:',name, ',' ' всего задач' ':',count_DZ,'затрачено часов:',take_time, ',' ' среднее время выполнения',task_time)
